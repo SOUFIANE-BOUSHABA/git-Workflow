8e41f8a406537d355db1c8a5f70fa492bee35684
SOUFIANE-BOUSHABA <soufianboushaba314@gmail.com>
2025-10-16 15:38:55 +0100

    FEAT :  this  commit is  for adding  version

--- App.java
+++ App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello v0.1");
         System.out.println("Args count: " + args.length);
+         String version = "0.3.0-SNAPSHOT"; 
++        System.out.println("Version: " + version);
     }
 }
