1711baedafcd73c1ecacb0799c18ea37f771a741
SOUFIANE-BOUSHABA <soufianboushaba314@gmail.com>
2025-10-17 12:18:41 +0100

    FIX: hotfix banner prefix

--- App.java
+++ App.java
@@ -2,11 +2,11 @@ public class App {
     public static void main(String[] args) {
 
         
-         System.out.println("=== Demo App ==="); 
+         System.out.println("[HOTFIX] === Demo App ===");
         System.out.println("Hello v0.1");
         System.out.println("Args count: " + args.length);
          String version = "0.3.0-SNAPSHOT"; 
-+        System.out.println("Version: " + version);
+        System.out.println("Version: " + version);
         System.out.println("Hello v1.1.0");
 
     }
